e871b56bd3540e20cae8d14732646a3bb6ac5171
CK Hicks <contact@ckhicks.com>
2014-05-04 17:19:14 -0500

    updating paths for scripts

--- responsimple/config.rb
+++ responsimple/config.rb
@@ -5,7 +5,7 @@ http_path = "/"
 css_dir = "/"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "js"
+javascripts_dir = "scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
