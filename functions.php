6e5c5c2c73e8e24f1bb92ba49a94cf59d346b9c8
CK Hicks <contact@ckhicks.com>
2014-09-02 14:40:19 -0500

    updating grid structure, jquery embed

--- functions.php
+++ functions.php
@@ -10,10 +10,9 @@
 //  This allows loading jQuery into the header or footer, plus controlling which version is used, if needed.
 //
 	add_action( 'wp_enqueue_scripts', 'proper_wp_jquery' );
-
 	function proper_wp_jquery(){
 		wp_deregister_script('jquery');
-		wp_register_script('jquery', (get_template_directory_uri() . '/scripts/jquery.js'), false, '1.11.0', true);
+		wp_register_script('jquery', (get_template_directory_uri() . '/scripts/jquery-1.11.1.js'), false, '1.11.1', true);
 		wp_enqueue_script('jquery');
 	}
 
